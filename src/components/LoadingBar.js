25279f0ed53b704b5c63b0287a89958616ff76cb
Jiyun <12jiyun@gmail.com>
2024-09-30 10:40:53 +0900

    remove all

--- src/components/LoadingBar.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class LoadingBar extends Component {
-  render() {
-    return <div id="loading"></div>;
-  }
-}
-
-export default LoadingBar;
--- src/pages/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class ErrorPage extends Component {
-  render() {
-    return <div>404 error</div>;
-  }
-}
-
-export default ErrorPage;
