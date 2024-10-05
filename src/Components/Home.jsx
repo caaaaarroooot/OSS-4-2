d42ef9b41d25ecdfcf157e6dee9a27c29f2716d9
Jiyun <12jiyun@gmail.com>
2024-10-06 00:15:13 +0900

    final

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Typography } from "@material-ui/core";
 import styled from "styled-components";
-// 6. styled components 적용, Typography 의 variant값 조정 및 가운데 정렬
+// 6. styled components 적용, Typography 의 variant값 조정 및 가운데 정렬햣
 const BoxDiv = styled.div`
     margin: 40px 0;
     height: 80vh;
